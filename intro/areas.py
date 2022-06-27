d882572571bad0445d5125d6754e5ed595418cf2
Fabian Morales Astorga <fmorales.astorga.15@gmail.com>
2022-06-26 23:15:24 -0400

    subiendo funcion areas

---
+++ intro/areas.py
@@ -0,0 +1,12 @@
+def area_triangulo(altura, base):
+  a_triangulo = (altura * base)/2
+  print(a_triangulo)
+
+def area_circulo(radio):
+  a_circulo = 3.14159 * radio
+  print(a_circulo)
+
+
+area_triangulo(6, 9)
+
+area_circulo(8)
