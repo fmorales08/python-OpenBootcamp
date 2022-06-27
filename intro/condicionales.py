76e431897294efddd32d0f26833befe2846e6378
Fabian Morales Astorga <fmorales.astorga.15@gmail.com>
2022-06-26 22:29:17 -0400

    subiendo tareas condicionales y while

---
+++ intro/condicionales.py
@@ -0,0 +1,7 @@
+edad = int(input("Ingrese su edad: "))
+# En Chile la mayoria de edad comienza a los 18 años
+if edad >= 18:
+  print ("Es mayor de edad")
+else:
+  print("Es menor de edad")
+
---
+++ intro/while.py
@@ -0,0 +1,7 @@
+num_inicial = 2
+num_final = 12
+
+while num_inicial < num_final:
+  num_inicial += 1
+  if num_inicial % 2 == 1:
+    print(num_inicial)
